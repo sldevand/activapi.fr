c51c4bbf0ed7f784e454559780a7f964ee776931
Sébastien Lorrain <sldeveloperand@gmail.com>
2019-03-24 19:11:21 +0100

    tests: finished scenariosequence tests

--- Tests/Model/Scenarios/ScenarioSequenceManagerPDOTest.php
+++ Tests/Model/Scenarios/ScenarioSequenceManagerPDOTest.php
@@ -6,9 +6,12 @@ use Entity\Scenario\ScenarioSequence;
 use Model\Scenario\ScenarioSequenceManagerPDO;
 use Tests\Model\AbstractManagerPDOTest;
 
+/**
+ * Class ScenarioSequenceManagerPDOTest
+ * @package Tests\Model\Scenarios
+ */
 class ScenarioSequenceManagerPDOTest extends AbstractManagerPDOTest
 {
-
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
@@ -20,17 +23,16 @@ class ScenarioSequenceManagerPDOTest extends AbstractManagerPDOTest
         self::executeSqlScript(SCENARIOS_SQL_PATH);
     }
 
-
     /**
      * @dataProvider saveProvider
-     * @param ScenarioSequence $entity
+     * @param ScenarioSequence $scenarioSequence
      * @param ScenarioSequence $expected
      * @throws \Exception
      */
-    public function testSave($entity, $expected)
+    public function testSave($scenarioSequence, $expected)
     {
         $manager = $this->getManager();
-        $manager->save($entity);
+        $manager->save($scenarioSequence);
         $persisted = $manager->getUnique($expected->id());
 
         self::assertEquals($expected, $persisted);
@@ -39,34 +41,93 @@ class ScenarioSequenceManagerPDOTest extends AbstractManagerPDOTest
         self::assertTrue($res == 1);
     }
 
-    public function testGetAll($entities, $expected)
+    /**
+     * @dataProvider getAllProvider
+     * @param ScenarioSequence[] $scenarioSequences
+     * @param ScenarioSequence[] $expected
+     * @throws \Exception
+     */
+    public function testGetAll($scenarioSequences, $expected)
     {
-        // TODO: Implement testGetAll() method.
+        self::dropAndCreateTables();
+        $manager = $this->getManager();
+        foreach ($scenarioSequences as $scenarioSequence) {
+            $manager->save($scenarioSequence);
+        }
+        $persisted = $manager->getAll();
+        self::assertEquals($expected, $persisted);
     }
 
-    public function testDelete($entity, $expected)
+    /**
+     * @dataProvider deleteProvider
+     * @param ScenarioSequence $scenarioSequence
+     * @param ScenarioSequence $expected
+     * @throws \Exception
+     */
+    public function testDelete($scenarioSequence, $expected)
     {
-        // TODO: Implement testDelete() method.
+        self::dropAndCreateTables();
+
+        $manager = $this->getManager();
+        $manager->save($scenarioSequence);
+        $persisted = $manager->getUnique($expected->id());
+        self::assertEquals($expected, $persisted);
+        $manager->delete($expected->id());
+        $persisted = $manager->getUnique($expected->id());
+        self::assertNotEquals($expected, $persisted);
     }
 
+    /**
+     * @return ScenarioSequence[]
+     */
     public function saveProvider()
     {
         return [
             "createScenarioSequence" => [
                 $this->makeScenarioSequence(1, 1),
                 $this->makeScenarioSequence(1, 1, 1)
+            ],
+            "updateScenarioSequence" => [
+                $this->makeScenarioSequence(1, 2, 1),
+                $this->makeScenarioSequence(1, 2, 1)
             ]
         ];
     }
 
+    /**
+     * @return ScenarioSequence[]
+     */
     public function getAllProvider()
     {
-        // TODO: Implement getAllProvider() method.
+        return [
+            "createScenarioSequences" => [
+                [
+                    $this->makeScenarioSequence(1, 1),
+                    $this->makeScenarioSequence(1, 2),
+                    $this->makeScenarioSequence(1, 3),
+                    $this->makeScenarioSequence(1, 4)
+                ],
+                [
+                    $this->makeScenarioSequence(1, 1, 1),
+                    $this->makeScenarioSequence(1, 2, 2),
+                    $this->makeScenarioSequence(1, 3, 3),
+                    $this->makeScenarioSequence(1, 4, 4)
+                ]
+            ]
+        ];
     }
 
+    /**
+     * @return ScenarioSequence[]
+     */
     public function deleteProvider()
     {
-        // TODO: Implement deleteProvider() method.
+        return [
+            "deleteScenarioSequence" => [
+                $this->makeScenarioSequence(1, 1),
+                $this->makeScenarioSequence(1, 1, 1)
+            ]
+        ];
     }
 
     /**
