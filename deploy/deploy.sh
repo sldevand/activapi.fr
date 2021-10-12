2e0b3cf0b667e5d8eff9f72b8e25e9ce9406600b
sldevand <sldeveloperand@gmail.com>
2021-10-12 18:06:02 +0200

    add env vars to deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
 
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-function myEcho(){
+function myEcho() {
     echo ""
-    echo "${green}--> $1 ${reset}"
+    echo "${green}*** $1 ***${reset}"
 }
 
-#Local vars
-LOCAL_WWW_PATH=/var/www
-LOCAL_APP_NAME=activapi.fr
+function dotenv () {
+  set -a
+  [ -f .env ] && . .env
+  set +a
+}
+
+dotenv
+
 LOCAL_APP_PATH=$LOCAL_WWW_PATH/$LOCAL_APP_NAME
 LOCAL_BUILD_PATH=$LOCAL_APP_PATH/build
 LOCAL_REPO_PATH=$LOCAL_APP_PATH/build/$LOCAL_APP_NAME
-
-#Git vars
-GIT_PATH=https://github.com/sldevand/activapi.fr.git
-GIT_BRANCH=master
-
-#Remote vars
-REMOTE_HOST=pi@domusbox
-REMOTE_WWW_PATH=/var/www
-REMOTE_TMP_PATH=/home/pi/tmp
-REMOTE_APP_NAME=activapi.fr
 REMOTE_SCRIPT_PATH=$LOCAL_APP_PATH/deploy/remote/commands.sh
 
-myEcho "***START $LOCAL_APP_NAME deployer script START***"
+eval "$(ssh-agent -s)" &&
+ssh-add $SSH_PRIVATE_KEY &&
 
-myEcho "Launch tests"
+myEcho "START $LOCAL_APP_NAME deployer script START"
+
+myEcho "Local : Launch tests"
 php $LOCAL_APP_PATH/vendor/bin/phpunit --configuration $LOCAL_APP_PATH/Tests/phpunit.xml &&
 
-myEcho "Local : Build App" &&
+myEcho "Local :  Build App" &&
 rm -rvf $LOCAL_BUILD_PATH &&
 mkdir $LOCAL_BUILD_PATH &&
 
@@ -40,9 +40,9 @@ myEcho "Local : Git clone $GIT_BRANCH branch" &&
 cd $LOCAL_BUILD_PATH &&
 git clone --single-branch --branch $GIT_BRANCH $GIT_PATH &&
 
-myEcho "Sassify scss files" &&
+myEcho "Local : Sassify scss files" &&
 npm run sass-compile &&
-myEcho "Babelify all js files" &&
+myEcho "Local : Babelify all js files" &&
 npm run babelAll &&
 
 cd $LOCAL_APP_PATH &&
@@ -67,13 +67,12 @@ rm -rfv $LOCAL_REPO_PATH/Web/get_oauth_token.php &&
 rm -rfv $LOCAL_REPO_PATH/.env.sample &&
 rm -rfv $LOCAL_REPO_PATH/.env &&
 
-ssh-add ~/.ssh/domusbox_rsa &&
 
-myEcho "***Remote : copy from local $LOCAL_REPO_PATH to remote $REMOTE_HOST:$REMOTE_TMP_PATH***" &&
+myEcho "Remote : copy from local $LOCAL_REPO_PATH to remote $REMOTE_HOST:$REMOTE_TMP_PATH" &&
 scp -r $LOCAL_REPO_PATH $REMOTE_HOST:$REMOTE_TMP_PATH &&
 ssh $REMOTE_HOST 'bash -s' < $REMOTE_SCRIPT_PATH &&
 
-myEcho "***Local : remove build files***" &&
+myEcho "Local : remove build files" &&
 rm -rf $LOCAL_BUILD_PATH &&
 
-myEcho "***END $LOCAL_APP_NAME deployer script END***"
+myEcho "END $LOCAL_APP_NAME deployer script END"
